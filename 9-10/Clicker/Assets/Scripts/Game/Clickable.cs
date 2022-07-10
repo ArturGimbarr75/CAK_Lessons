cb667bae1306d2b896a16a7df35225bbc1032f88
Artur <artur.radzivilov@stud.vilniustech.lt>
2022-07-10 14:33:49 +0300

    1_4 Lesson
    1. Adding colliders and new script
    2. Change new script params
    3. Creating clickables prefabs

---
+++ 9-10/Clicker/Assets/Scripts/Game/Clickable.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clickable : MonoBehaviour
+{
+    [SerializeField] private AnimationCurve _scaleCurve;
+    [SerializeField, Min(0)] private float _timeToClick;
+
+    IEnumerator Start()
+    {
+        WaitForEndOfFrame wait = new WaitForEndOfFrame();
+
+        for (float time = 0; time < _timeToClick; time += Time.deltaTime)
+        {
+            transform.localScale = Vector3.one * _scaleCurve.Evaluate(time / _timeToClick);
+            yield return wait;
+        }
+        //Score.Instance.MissClick();
+        Destroy(gameObject);
+    }
+
+    private void OnMouseDown()
+    {
+        //Score.Instance.Click();
+        Destroy(gameObject);
+    }
+}
