2c10b1febc65f72069dd6be3caee58a04bc81331
Artur <artur.radzivilov@stud.vilniustech.lt>
2022-07-11 01:25:02 +0300

    2_11 Lesson
    Updated enemy

--- 9-10/DoodleJump/Assets/Scripts/Game/Enemy.cs
+++ 9-10/DoodleJump/Assets/Scripts/Game/Enemy.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField, Min(0)] private float _additionalHeight;
+
+    private const float INFELICITY = 0.1f;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform == Player.Instance)
         {
-            Player.Instance.GetComponent<Collider2D>().enabled = false;
-            Player.Instance.GetComponent<PlayerHorizontalMovement>().enabled = false;
-            Player.Instance.GetComponent<Rigidbody2D>().velocity = Vector2.zero;            
+            if (Player.Instance.position.y + INFELICITY < transform.position.y)
+            { 
+                Player.Instance.GetComponent<Collider2D>().enabled = false;
+                Player.Instance.GetComponent<PlayerHorizontalMovement>().enabled = false;
+                Player.Instance.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            }
+            else
+            {
+                Rigidbody2D rigidbody = Player.Instance.GetComponent<Rigidbody2D>();
+                float impulse = Mathf.Sqrt(_additionalHeight * -2 *
+                    (Physics2D.gravity.y * rigidbody.gravityScale));
+                rigidbody.velocity = Vector2.zero;
+                rigidbody.AddForce(Vector2.up * impulse, ForceMode2D.Impulse);
+                gameObject.AddComponent<Rigidbody2D>().angularVelocity = Random.Range(360, 720);
+                Destroy(GetComponent<Collider2D>());
+            }
         }
     }
 }
