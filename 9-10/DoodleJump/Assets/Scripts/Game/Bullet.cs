15075b13f4fe988367bf9d49e264d05bf78320e4
Artur <artur.radzivilov@stud.vilniustech.lt>
2022-07-11 02:51:36 +0300

    2_15 Lesson
    1. Shooting and Bullet scripts 
    2. Bullet prefab
    3. Builds

---
+++ 9-10/DoodleJump/Assets/Scripts/Game/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Bullet : MonoBehaviour
+{
+    [SerializeField, Range(1f, 10f)] private float _beginImpulse;
+    [SerializeField, Min(1)] private float _lifeTime;
+
+    void Start()
+    {
+        Destroy(gameObject, _lifeTime);
+        GetComponent<Rigidbody2D>().AddForce(Vector3.up * _beginImpulse, ForceMode2D.Impulse);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out Enemy _))
+        {
+            Destroy(gameObject);
+            Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ 9-10/DoodleJump/Assets/Scripts/Player/PlayerShoot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    [SerializeField] private GameObject _bulletPrefab;
+    [SerializeField, Min(0)] private float _cooldownTime;
+    [SerializeField, Min(0)] private float _verticalOffset;
+
+    private bool _canShoot = true;
+    
+    void Update()
+    {
+        if (_canShoot && Input.GetKeyDown(KeyCode.Space))
+        {
+            _canShoot = false;
+            Invoke(nameof(Cooldown), _cooldownTime);
+            Instantiate(_bulletPrefab).transform.position = transform.position
+                + Vector3.up * _verticalOffset;
+        }
+    }
+
+    void Cooldown() => _canShoot = true;
+}
